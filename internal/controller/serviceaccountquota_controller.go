66b3990cc5271ea2400fdd9e8bf241a5c51a85e1
Harry Li <bertram_li@163.com>
2025-08-20 19:27:25 +0800

    Add cert-gen job

--- internal/controller/serviceaccountquota_controller.go
+++ internal/controller/serviceaccountquota_controller.go
@@ -63,6 +63,7 @@ func NewServiceAccountResourceQuotaReconciler(mgr manager.Manager) *ServiceAccou
 // +kubebuilder:rbac:groups=quota.mayooot.github.io,resources=serviceaccountquotas/finalizers,verbs=update
 // +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 // +kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;create
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
